64bd73d80b56dbb812aa05de3b005da51c80eb65
adri.kriptok@gmail.com <zank_kriptok@hotmail.com>
2020-07-11 03:56:39 -0300

    Recuperar cambios perdidos en el merge.

--- _4_Proc.sql
+++ _4_Proc.sql
@@ -1,66 +1,79 @@
+DROP PROCEDURE IF EXISTS sp_TurnosDias;
+
+/*stored procedure auxiliar que agrega un cierto rango horario y cierta cantidad de dias */
+delimiter $$
+CREATE PROCEDURE sp_TurnosDias
+(
+	idAtencion INT,
+	cantidadDias INT,
+	hDesde TIME,
+	hHasta TIME	
+)
+BEGIN
+/* Declaracion de variables y valores por default. */
+    DECLARE it int DEFAULT 0;
+/* Bucle por cantidad de dias*/
+diasWhile : WHILE it < cantidadDias DO
+		SET @horaTurno = hDesde;
+		SET @diaTurno = DATE_ADD(@primerDia, INTERVAL it DAY);
+			/* Bucle por franja horaria*/
+			creaWhile : while @horaTurno < hHasta DO
+				INSERT INTO Turno (idAt, fecha, hora, dniPac) VALUES (idAtencion, @diaTurno, @horaTurno, NULL);
+				SET  @horaTurno = ADDTIME(@horaTurno, @duracionDelTurno);
+			END  while creaWhile;  
+	SET it = it + 1;
+	END WHILE diasWhile;
+END$$
+delimiter ;
+
+/*-------------------------------------------------*/
+
 /* Borro el stored procedure, si ya est� creado. */
-drop procedure IF EXISTS sp_CreaTurnos;
+DROP PROCEDURE IF EXISTS sp_CreaTurnos;
 
 /* Creo el nuevo stored procedure. */
-
 delimiter $$
-create procedure sp_CreaTurnos
+CREATE PROCEDURE sp_CreaTurnos
 (
 	idAtencion   int,
 	cantidadDias int
 )
-begin
+BEGIN
 	DECLARE done INT DEFAULT FALSE; /* Por workaround de un bug. */
-
-	/* Declaracion de variables y valores por default. */
-    declare it int default 0;
+	
 	/* Horarios en los que atiende el medico */
-    declare hDesde, hHasta time;
-
+    DECLARE hDesde, hHasta time;
+   
 	/* Un medico puede tener mas de un horario de atencion. */
-    DECLARE cursorHorario CURSOR FOR SELECT horaDesde, horaHasta from HorarioAtencion where idAt = idAtencion;
+    DECLARE cursorHorario CURSOR FOR SELECT horaDesde, horaHasta FROM HorarioAtencion WHERE idAt = idAtencion;
 
     DECLARE CONTINUE HANDLER FOR NOT FOUND SET done = 1; /* Por workaround de un bug. */
 
-    set @primerDia = DATE("1900-01-01");
 	/* Para la atenci�n del par�metro,  buscamos el �ltimo turno que tenga.*/
     /* Buscamos la fecha del �ltimo turno, y le sumamos un d�a, para saber desde d�nde arrancar. */
-	select DATE_ADD( IFNULL(Max(fecha), CURDATE()), INTERVAL 1 DAY) into @primerDia from Turno where idAt = 1;
-
-    /* Abro cursor para iterar por los horarios de atencion del medico. */
+    # Se tiene que settear un fecha fija para que sea deterministico 
+	SELECT DATE_ADD( IFNULL(Max(fecha),DATE("1900-01-01")), INTERVAL 1 DAY) INTO @primerDia FROM Turno WHERE idAt = idAtencion;
+	/*Cada idAt tiene una duracion del turno asignada*/
+	SELECT duracionTurno INTO @duracionDelTurno FROM atencion WHERE idAt = idAtencion;
+		 
+	 /* Abro cursor para iterar por los horarios de atencion del medico. */
 	OPEN cursorHorario;
 
 	/* Por cada horario de atencion del medico */
     cursorLoop: LOOP
+    
 		/* los horarios de atencion del medico */
 		FETCH cursorHorario INTO hDesde, hHasta;
-        
+      
 		/* Por workaround de un bug. */
 		IF done = 1 THEN LEAVE cursorLoop; END IF;
-
-		set @horaTurno = hDesde;
-      
-		set it = 0; /* Reseteo el iterador. */ 
-        
-		/* Itero por la cantidad de días. */    
-		diasWhile : WHILE it < cantidadDias DO
-			/* Por cada día, genero los turnos correspondientes. */
-			set @diaTurno = DATE_ADD(@primerDia, INTERVAL it DAY);                        			
-            
-            creaWhile : while @horaTurno < hHasta do            			
-				insert into turno (idAt, fecha, hora, dniPac)
-				values            (idAtencion, @diaTurno, @horaTurno, null);
-				            
-				set @horaTurno = ADDTIME(@horaTurno, '00:05:00');
-			end while creaWhile;
-		set it = it + 1;
-		END WHILE diasWhile;
-    END LOOP;
-
-    /* Cierro cursor. */
-    CLOSE cursorHorario;
-end$$
+	/* Llamada al procedimiento auxiliar para la generacion de horarios por franja horaria*/
+	CALL sp_TurnosDias(idAtencion,cantidadDias,hDesde,hHasta);
+	END LOOP;
+	 /* Cierro cursor. */
+	CLOSE cursorHorario;    
+END$$
 delimiter ;
 
 /* Lo ejecuto para probar. */
-CALL sp_CreaTurnos(1 /* id-atencion */, 2 /* d�as */);
+CALL sp_CreaTurnos(1 /* id-atencion */, 3 /* d�as */);
